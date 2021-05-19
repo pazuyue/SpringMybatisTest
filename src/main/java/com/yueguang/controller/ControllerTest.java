acc40cab833ef616c8e31d4646d32495c26829bf
yueguang <yueguang@qingmutec.com>
2021-05-19 23:32:53 +0800

    第一版更新

--- src/main/java/com/yueguang/controller/ControllerTest.java
+++ src/main/java/com/yueguang/controller/ControllerTest.java
@@ -1,12 +1,16 @@
 package com.yueguang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ControllerTest implements Controller {
+@Controller
+public class ControllerTest{
 
+    @RequestMapping(value = "controllerTest")
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         System.out.println("测试");
         ModelAndView m = new ModelAndView();

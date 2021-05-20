3bc2426f5de974786dfd5d14326d7219ff042ffd
yueguang <yueguang@qingmutec.com>
2021-05-20 14:33:00 +0800

    POJO类测试

---
+++ src/main/java/com/yueguang/model/Banji.java
@@ -0,0 +1,11 @@
+package com.yueguang.model;
+
+import lombok.Data;
+
+@Data
+public class Banji {
+    private Integer banji_id;
+    private String banji_name;
+
+
+}
---
+++ src/main/java/com/yueguang/model/Student.java
@@ -0,0 +1,10 @@
+package com.yueguang.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private Integer stu_id;
+    private String stu_name;
+    private Banji banji;
+}

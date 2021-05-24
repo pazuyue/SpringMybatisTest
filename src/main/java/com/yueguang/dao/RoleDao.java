f2f9f196c0e73264ab767f0021e3c7baa6d2a59c
yueguang <yueguang@qingmutec.com>
2021-05-24 17:39:08 +0800

    新闻发布系统

---
+++ src/main/java/com/yueguang/dao/RoleDao.java
@@ -0,0 +1,9 @@
+package com.yueguang.dao;
+
+import com.yueguang.model.Role;
+
+import java.util.List;
+
+public interface RoleDao {
+    public List<Role> selectRoleList();
+}
--- src/main/java/com/yueguang/dao/UserDao.java
+++ src/main/java/com/yueguang/dao/UserDao.java
@@ -1,7 +1,32 @@
 package com.yueguang.dao;
 
 import com.yueguang.model.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface UserDao {
-    public User findUserById(Integer id);
+    //查询所有用户
+    public List<User> selectUserList(@Param("keywords") String keywords,@Param("userListRoledId") Integer userListRoledId);
+
+    //通过账号和密码查询用户
+    public User findUser(@Param("loginName") String loginName,@Param("password") String password);
+
+    //通过用户ID查询客户
+    public User findUserById(Integer userId);
+
+    //通过用户登陆名查询用户
+    public User getUserLoginName(String loginName);
+
+    //添加用户
+    public int addUser(User user);
+
+    //更新用户
+    public int updateUser(User user);
+
+    //删除用户
+    public int delUser(Integer userId);
+
+    //设置用户状态
+    public int setUserStatus(User user);
 }
